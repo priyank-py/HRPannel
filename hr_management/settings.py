7666063294f110a276f6d8eb65040f7cc3bd79f9
Priyank <ipriyank007@hotmail.com>
2020-01-20 12:44:23 +0530

    Update settings.py

--- hr_management/settings.py
+++ hr_management/settings.py
@@ -81,10 +81,20 @@ WSGI_APPLICATION = 'hr_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pgtestcrmdb',
+        'USER': 'postgres',
+        'PASSWORD': 'password123',
+        'HOST': 'localhost',
     }
 }
 
